f574c2f1f883ed887a9501f6c6dc172f2bd12b8a
Patryk Saganowski <87946056+psnoox@users.noreply.github.com>
2023-08-03 23:05:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+let sec = 10;
+let random;
+const stats = {
+    correct: 0,
+    incorrect: 0
+}
+function genRandom(){
+    const r1 = Math.floor(Math.random() * 10)+1;
+    const r2 = Math.floor(Math.random() * 10)+1; 
+    
+    return [r1, r2, r1 * r2];
+}
+function nextQuest(){
+    random = genRandom();
+    $("#quest").text(`${random[0]} \u2715 ${random[1]}`); 
+    sec = 10;
+}
+function correct(){
+    stats.correct += 1;
+    $("#correct").text(stats.correct);
+}
+function incorrect(){
+    stats.incorrect+= 1;
+    $("#incorrect").text(stats.incorrect);
+}
+function check(){
+    const result = parseInt($("#result").val());
+    if(result === random[2]){
+        correct();
+        nextQuest();
+    } else {
+        console.log(result, random)
+        incorrect();
+    }
+}
+$(document).ready(function (){
+    random = genRandom();
+    $("#quest").text(`${random[0]} \u2715 ${random[1]}`);
+    const interval = setInterval(function(){
+        if(sec === 0){
+            incorrect();
+            nextQuest();
+        }
+        $("#timer").text(`0:${sec}`);
+        sec -= 1;
+    }, 1000)
+    $("#checkResult").click(function(e) {
+        check();
+    })
+    $("#result").keypress(function(e) {
+        if(e.which == 13){
+            check();
+        }
+    })
+})
